23f6be8f5b2a2e206d3c91f2bf76e96443c36503
Jacob Rienstra <jacobr@fb.com>
2019-09-08 18:11:27 -0400

    multiple events, clean up code

--- scripts/background.js
+++ scripts/background.js
@@ -2,24 +2,32 @@ const rule1 = {
   conditions: [
     new chrome.declarativeContent.PageStateMatcher({
       pageUrl: {
-        hostContains: "broadwayworld",
-        pathContains: "equity-audition"
+        hostContains: 'broadwayworld',
+        pathContains: 'equity-audition'
       }
     })
   ],
   actions: [new chrome.declarativeContent.ShowPageAction()]
-};
+}
 
-chrome.runtime.onInstalled.addListener(function(details) {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([rule1]);
-  });
-});
+chrome.runtime.onInstalled.addListener(function (details) {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([rule1])
+  })
+})
 
 // Called when the user clicks on the browser action.
-chrome.pageAction.onClicked.addListener(function(tab) {
+chrome.pageAction.onClicked.addListener(function (tab) {
   // No tabs or host permissions needed!
-  chrome.tabs.executeScript({ file: "scripts/ics.deps.min.js" }, function() {
-    chrome.tabs.executeScript({ file: "scripts/grabber.js" });
-  });
-});
+  chrome.tabs.executeScript(
+    { file: 'scripts/jquery-3.4.1.slim.min.js' },
+    function () {
+      chrome.tabs.executeScript(
+        { file: 'scripts/ics.deps.min.js' },
+        function () {
+          chrome.tabs.executeScript({ file: 'scripts/grabber.js' })
+        }
+      )
+    }
+  )
+})
--- scripts/grabber.js
+++ scripts/grabber.js
@@ -1,80 +1,129 @@
 // ES6
+// Get all the sibling tags until some tag, filtering out filter
+var nextUntil = function(node, until, filter) {
+  var siblings = [];
+  while ((node = node.nextSibling) && node.nodeType !== 9) {
+    // You can't skip text nodes
+    if (node.nodeType === 3) {
+      // No empty text nodes
+      if (!/^\s*\n*$/.test(node.textContent)) siblings.push(node);
+    }
+    if (node.nodeType === 1) {
+      if (until && node.matches(until)) {
+        break;
+      }
+      if (filter && node.matches(filter)) {
+        continue;
+      }
+      siblings.push(node);
+    }
+  }
+  return siblings;
+};
+
+var others = ["PREPARATION", "SEEKING", "CONTRACT", "PERSONNEL"];
 
 var headers = document.getElementsByTagName("strong");
-
+var dateRegex = /\b(?:(?:Mon)|(?:Tues?)|(?:Wed(?:nes)?)|(?:Thur?s?)|(?:Fri)|(?:Sat(?:ur)?)|(?:Sun))(?:day)?\b[:\-,]?\s*(?:(?:jan|feb)?r?(?:uary)?|mar(?:ch)?|apr(?:il)?|may|june?|july?|aug(?:ust)?|oct(?:ober)?|(?:sept?|nov|dec)(?:ember)?)\s+\d{1,2}\s*,?\s*\d{4}/i;
+var timeRegex = /^(?:((?:1[0-2]|0?[1-9])(?::(?:[0-5][0-9]))?(?:\s*)(?:[ap]m)?(?:\s*(?:-?|(?:to)?)\s*)?(?:1[0-2]|0?[1-9])(?::(?:[0-5][0-9]))?(?:\s*)(?:[ap]m)?)(?:\s*)(\([a-z]{3}\)))/i;
 var date = (start_time = end_time = null);
-var lunch = (subject = desc = seeking = prep = loc = "");
+var subject = (loc = desc = "");
 
-function getNextTextNode(node) {
-  node = node.nextSibling;
-  while (
-    node !== null &&
-    ((node.nodeType !== 3 && node.nodeName != "STRONG") ||
-      (node.nodeType === 3 && !/\S/.test(node.nodeValue)))
-  ) {
-    node = node.nextSibling;
-  }
-  return node;
-}
-
+// Assume the title is in the first <strong> tag
 var title = headers[0];
-subject += title.innerHTML.trim() + " ";
-filename = title.innerHTML
-  .replace(/\s/g, "")
-  .toLowerCase()
-  .trim();
-title = getNextTextNode(title);
-do {
-  subject += title.wholeText.trim();
-  subject += " ";
-  title = getNextTextNode(title);
-} while (title !== null && title.nodeName != "STRONG");
-
+subject += title.textContent.trim() + " "; // bold text
+subject += nextUntil(title, "strong", "br")
+  .map(node => node.textContent.trim())
+  .join(" ");
 for (let header of headers) {
-  switch (header.innerHTML) {
+  switch (header.textContent) {
     case "AUDITION DATE":
-      var node = getNextTextNode(header);
-      date = node.wholeText.trim();
-      node = getNextTextNode(node);
-      var times = node.wholeText.trim();
-      var [start, end] = times.split("-");
-      start_time = new Date(date + " " + start);
-      end_time = new Date(date + " " + end);
-      lunch = getNextTextNode(node).wholeText.trim();
+      data = nextUntil(header, "strong", "br");
+      dates = [];
+      other = [];
+      let curDate = "";
+      // iterate through date data
+      for (let line of data) {
+        line = line.textContent.trim();
+        // if date, reset curDate str to it
+        if (dateRegex.test(line)) {
+          curDate = line.trim();
+          continue;
+        }
+        // if time & we have date, add start & end
+        if (timeRegex.test(line) && curDate !== "") {
+          var matches = line.match(timeRegex);
+          var [start, end] = matches[1].split("-").map(e => e.trim());
+          var tz = "";
+          if (matches.length == 3) {
+            tz = matches[2];
+          }
+          dates.push({
+            start: [curDate, start, tz].join(" "),
+            end: [curDate, end, tz].join(" ")
+          });
+          continue;
+        } else {
+          // probably lunch info but who knows
+          other.push(line.trim());
+        }
+      }
+      dates = dates.map(function(dateDict) {
+        return { start: new Date(dateDict.start), end: new Date(dateDict.end) };
+      });
+      desc += other.join("\\n");
       break;
     case "LOCATION":
-      var node = getNextTextNode(header);
-      do {
-        loc += node.wholeText.trim();
-        loc += ", ";
-        node = getNextTextNode(node);
-      } while (node.nodeName != "STRONG");
-      break;
-    case "PREPARATION":
-      var node = getNextTextNode(header);
-      prep = node.wholeText.trim(); //while
-      break;
-    case "SEEKING":
-      var node = getNextTextNode(header);
-      seeking = node.wholeText.trim(); //while
+      loc = nextUntil(header, "strong", "br")
+        .map(node => node.textContent.trim())
+        .join(" ");
       break;
     default:
+      if (!others.includes(header.textContent)) break;
+      if (desc != "") desc += "\\n";
+      desc +=
+        header.textContent +
+        "\\n" +
+        nextUntil(header, "strong", "br")
+          .map(node => node.textContent.trim())
+          .join(" ")
+          .replace("\n", "\\n");
       break;
   }
 }
-desc += "PREPARATION:\\n" + prep + "\\n\\nSEEKING:\\n" + seeking + "\\n\\n";
 
-function escape(string) {
+var escapeCommas = function(string) {
   return string.replace(/[,\/]/g, "\\,");
+};
+
+var camelize = (text, separator = " ") => {
+  const words = text.split(separator);
+  const result = [];
+  words.forEach(word =>
+    result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+  );
+  return result.join("");
+};
+
+function foldLine(line) {
+  const parts = [];
+  while (line.length > 50) {
+    parts.push(line.slice(0, 50));
+    line = line.slice(50);
+  }
+  parts.push(line);
+  return parts.join("\r\n ");
 }
 
+// desc = foldLine(desc);
+// loc = foldLine(loc);
+var url = window.location.href;
+// url = foldLine(url);
+
 var cal = ics();
-cal.addEvent(
-  subject,
-  desc,
-  escape(loc),
-  start_time,
-  end_time,
-  window.location.href
-);
+for (date of dates) {
+  cal.addEvent(subject, desc, escapeCommas(loc), date.start, date.end, url);
+}
+
+var filename = camelize(subject.replace(/[^a-z\s]/gi, "")).slice(0, 25);
 cal.download(filename);
